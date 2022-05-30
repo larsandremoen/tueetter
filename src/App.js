34e1d0f7a66316a0bbed44d09f1033f380f7aa68
Lars André Møen <post@larsandre.no>
2022-05-30 20:36:43 +0200

    💩 init. design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+
+const InputField = ({ setText, text }) => {
+  return (
+    <div>
+      <div
+        style={{
+          width: "70%",
+          flexDirection: "column",
+          flex: 1,
+          border: "1px solid",
+          borderRadius: "5px",
+          margin: "0px",
+        }}
+      >
+        <textarea
+          value={text}
+          maxLength={140}
+          placeholder={"What's on your mind?"}
+          style={{ flex: 1, border: "none", minHeight: "150px" }}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const [tueet, setTueet] = useState("");
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="custom-field">
+      <div style={{ flexGrow: 1 }}></div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          borderRadius: "5px",
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "200px",
+          padding: "20px",
+        }}
+      >
+        <InputField text={text} setText={setText} />
+
+        <div
+          id="borderBottom"
+          style={{
+            marginTop: 10,
+            width: "100%",
+            height: 20,
+            borderBottom: "1px solid #dedede",
+          }}
+        />
+      </div>
+      <div style={{ flex: 1 }}>
+        <div
+          style={{
+            margin: 5,
+            backgroundColor: "red",
+            height: 200,
+            width: "70%",
+          }}
+        ></div>
+
+        <div
+          style={{
+            margin: 5,
+            backgroundColor: "red",
+            height: 200,
+            width: "70%",
+          }}
+        ></div>
+        <div
+          style={{
+            margin: 5,
+            backgroundColor: "red",
+            height: 200,
+            width: "70%",
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
